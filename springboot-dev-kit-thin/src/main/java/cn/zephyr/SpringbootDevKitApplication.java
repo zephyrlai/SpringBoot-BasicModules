205e7084ede2f59b18841671a20c7999ed0aa82e
zephyrLai <laizonghao@foxmail.com>
2018-08-03 14:30:27 +0800

    新增springboot-dev-kit-thin模块

---
+++ springboot-dev-kit-thin/src/main/java/cn/zephyr/SpringbootDevKitApplication.java
@@ -0,0 +1,12 @@
+package cn.zephyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDevKitApplication.class, args);
+    }
+}
---
+++ springboot-dev-kit-thin/src/main/java/cn/zephyr/module/controller/BaseController.java
@@ -0,0 +1,24 @@
+package cn.zephyr.module.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Title: SpringBoot-BasicModules
+ * @Package: cn.zephyr.module.controller
+ * @Description:
+ * @author: Lai
+ * @date 2018/8/3 11:40
+ */
+@Controller
+public class BaseController {
+
+    @RequestMapping("/")
+    public ModelAndView index(Model model){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
+    }
+}
